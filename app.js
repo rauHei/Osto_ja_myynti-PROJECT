d9334dae8b3da29d29ae9ffa86a11ed54e2908c2
rauHei <101550372+rauHei@users.noreply.github.com>
2022-08-25 11:50:47 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+import express, { urlencoded, json } from 'express';
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+import { create } from 'express-handlebars';
+import cookieParser from 'cookie-parser';
+import pages from './routes/page.routes.js';
+import register from './routes/register.routes.js';
+import auth from './routes/auth.routes.js';
+import hbsHelpers from './helpers/handlebars-helpers.js';
+
+dotenv.config();
+
+
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT
+
+});
+//console.log(db);
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "ostaJamyy",
+
+
+// });
+
+const app = express();
+
+app.use(express.static('public'));
+
+const hbs = create({
+    extname: '.hbs',
+    helpers: hbsHelpers
+});
+
+app.engine('hbs', hbs.engine);
+
+app.set('view engine', 'hbs');
+
+app.use(urlencoded({ extended: false }));
+
+app.use(json());
+
+app.use(cookieParser());
+
+app.use('/', pages);
+app.use('/register', register);
+app.use('/auth', auth);
+
+app.listen(3030, () => {
+    console.log("Server started on port 3030");
+    db.connect((error) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Connected to database");
+        }
+    })
+});
+
+export default db;
